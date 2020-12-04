3fd5d6fa1ee9e4919991a0acd5577cfcb4317573
Filip <31472895+Filiq@users.noreply.github.com>
2020-12-04 17:01:29 +0100

    Add files via upload

--- 2. Progress Steps/script.js
+++ 2. Progress Steps/script.js
@@ -33,7 +33,6 @@ function update() {
 
   const actives = document.querySelectorAll(".active");
 
-  console.log(`${((actives.length - 1) / (circles.length - 1)) * 100}%`);
   progress.style.width = `${
     ((actives.length - 1) / (circles.length - 1)) * 100
   }%`;
